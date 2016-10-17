2a72765fdd80344e2a6b33c1f4433e28076da078
Soren Mathiasen <smo@tradeshift.com>
2016-10-17 09:16:02 +0200

    Initial commit

---
+++ client/client.go
@@ -0,0 +1,121 @@
+package client
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type CreateZone struct {
+	Name        string   `json:"name"`
+	Kind        string   `json:"kind"`
+	Masters     []string `json:"masters"`
+	Nameservers []string `json:"nameservers"`
+}
+
+type RecordSet struct {
+	Name       string   `json:"name"`
+	Type       string   `json:"type"`
+	TTL        int      `json:"ttl"`
+	Changetype string   `json:"changetype"`
+	Records    []Record `json:"records"`
+}
+type Record struct {
+	Content  string `json:"content"`
+	Disabled bool   `json:"disabled"`
+}
+type CreateRecord struct {
+	Rrsets []RecordSet `json:"rrsets"`
+}
+
+type pdnsclient struct {
+	// baseURL is the url for the powerdns host like http://localhost:8081
+	baseURL string
+	apiKey  string
+}
+
+func NewClient(baseURL string, apiKey string) *pdnsclient {
+	return &pdnsclient{apiKey: apiKey, baseURL: baseURL}
+}
+
+func (c *pdnsclient) AddZone(name string, nameServers []string) error {
+	cl := CreateZone{
+		Name:        name,
+		Kind:        "Native",
+		Masters:     []string{},
+		Nameservers: nameServers,
+	}
+	b, err := json.Marshal(cl)
+	if err != nil {
+		log.Println(err)
+		return errors.New("failure parsing zone struct to json")
+	}
+
+	url := c.baseURL + "/api/v1/servers/localhost/zones"
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(b))
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	req.Header.Add("X-API-Key", c.apiKey)
+	client := http.DefaultClient
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return errors.New(fmt.Sprintf("HTTP call returned %v", err))
+
+	}
+
+	if resp.StatusCode != 201 {
+		return errors.New(fmt.Sprintf("HTTP call returned %v", resp.StatusCode))
+
+	}
+	return nil
+
+}
+
+func (c *pdnsclient) AddRecord(name, dnstype, content string, ttl int, zone string) error {
+
+	p := CreateRecord{
+		Rrsets: []RecordSet{
+			RecordSet{Name: name, Type: dnstype, TTL: ttl, Changetype: "REPLACE",
+				Records: []Record{
+					Record{
+						Content:  content,
+						Disabled: false,
+					},
+				}},
+		},
+	}
+
+	b, err := json.Marshal(p)
+	if err != nil {
+		log.Println(err)
+		return errors.New("failure parsing record struct to json")
+	}
+
+	url := c.baseURL + "/api/v1/servers/localhost/zones/" + zone
+	req, err := http.NewRequest("PATCH", url, bytes.NewBuffer(b))
+	if err != nil {
+		log.Fatal("Error creating request", err)
+		return err
+	}
+	req.Header.Add("X-API-Key", c.apiKey)
+	client := http.DefaultClient
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return errors.New(fmt.Sprintf("HTTP call returned %v", err))
+
+	}
+
+	if resp.StatusCode != 204 && resp.StatusCode != 200 { // 204 No content = create, 200 = not updated but otherwise ok
+		return errors.New(fmt.Sprintf("HTTP call returned %v", resp.StatusCode))
+
+	}
+	return nil
+
+}
---
+++ client/client_test.go
@@ -0,0 +1,82 @@
+package client
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+)
+
+func TestCreateZoneBody(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		expectedPayload := `{"name":"example.org.", "kind": "Native", "masters": [], "nameservers": ["ns1.example.org.", "ns2.example.org."]}`
+		data, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			t.Error("Unable to read body")
+		}
+
+		if AreEqualJSON(string(data), expectedPayload) {
+			t.Errorf("'%v' should be equal to '%v'\n", expectedPayload, string(data))
+		}
+		w.WriteHeader(201)
+	}))
+	defer ts.Close()
+	c := NewClient(ts.URL, "changeme")
+	err := c.AddZone("example.org.", []string{"ns1.example.org", "ns2.example.org"})
+	if err != nil {
+		t.Error(err)
+	}
+
+}
+func TestCreateZoneHeader(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		apiKey := r.Header.Get("X-API-Key")
+		if apiKey != "changeme" {
+			t.Error("The API key was not configured correctly...")
+		}
+		w.WriteHeader(201)
+	}))
+	defer ts.Close()
+	c := NewClient(ts.URL, "changeme")
+	err := c.AddZone("example.org.", []string{"ns1.example.org", "ns2.example.org"})
+	if err != nil {
+		t.Error(err)
+	}
+
+}
+func TestEnsureCorrectCreateZoneURL(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		path := r.URL.String()
+		if path != "/api/v1/servers/localhost/zones" {
+			t.Errorf("'%v' should have been '%v'", path, "/api/v1/servers/localhost/zones")
+		}
+		w.WriteHeader(201)
+	}))
+	defer ts.Close()
+	c := NewClient(ts.URL, "changeme")
+	err := c.AddZone("example.org.", []string{"ns1.example.org", "ns2.example.org"})
+	if err != nil {
+		t.Error(err)
+	}
+
+}
+
+func AreEqualJSON(s1, s2 string) bool {
+	var o1 interface{}
+	var o2 interface{}
+
+	var err error
+	err = json.Unmarshal([]byte(s1), &o1)
+	if err != nil {
+		return false
+	}
+	err = json.Unmarshal([]byte(s2), &o2)
+	if err != nil {
+		return false
+	}
+
+	return reflect.DeepEqual(o1, o2)
+}
